1a1aa43b71492a152eba06de673b7f233b4fa25d
Tareq <tariqulislamtareq1670@gmail.com>
2026-09-09 11:57:40 +0600

    fix: omit image field (not null) when no profile picture uploaded

--- src/app/register/register-client.js
+++ src/app/register/register-client.js
@@ -94,7 +94,7 @@ export default function RegisterClient() {
           name,
           email,
           password,
-          image: imageUrl || null,
+          ...(imageUrl && { image: imageUrl }),
         }),
       });
 
